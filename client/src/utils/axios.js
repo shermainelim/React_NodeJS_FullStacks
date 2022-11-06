a8bc2a4dffb8cb435513a42430fbb09d37ff3385
shermaine <shermaine.lim.si.hui@gmail.com>
2022-11-06 16:09:09 +0800

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://mysql-deploy-heroku-2.herokuapp.com/",
+  baseURL: "https://mysql-deploy-heroku-2.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",

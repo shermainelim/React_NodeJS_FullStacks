9d44212ac954dda67d674201837046a64d9a57e4
shermaine <shermaine.lim.si.hui@gmail.com>
2022-11-06 16:54:51 +0800

    script

---
+++ mysql scripts/herokuMySql/admin.sql
@@ -0,0 +1,3 @@
+SELECT * FROM heroku_e3d09080d56a833.admin;
+
+INSERT INTO heroku_e3d09080d56a833.admin (fullName, username, password, email)VALUES ("Aimi Sey Le Mon" ,"AD01100","^([)-AKp$vgy*","aimiseylemon@finchocolatelabs.sg");
---
+++ mysql scripts/herokuMySql/residents.sql
@@ -0,0 +1,12 @@
+SELECT * FROM heroku_e3d09080d56a833.residents;
+
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("Mary Liang Jin Ling", "Blk 123, Jurong West St 12, Unit 01-20, S512123", "F123429111A", "9111A", "80002000");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("John Cai Jiang Su", "Blk 456, Jurong East St 45, Unit 02-20, S456456", "F223429211A", "9211A", "81112111");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("Razak bin Osman", "Blk 10, Eunos St 12, Unit 11-20, S0987010", "F323422311C", "2311C", "90002000");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("Nurul binte Musa", "Blk 8, Kallang St 8, Unit 10-201, S112008", "F423421541D", "1541D", "91112111");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("Nagaratnam s/o Suppiah", "Blk 30, Yishun St 12, Unit 10-2210, S212030", "F523421531E", "1531E", "82223000");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("Priya d/o Anandarajah", "Blk 40, Kranji St 12, Unit 20-201, S712040", "F523428111F", "8111F", "83332000");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("John Don Bach", "Blk 125, Dover St 9, Unit 02-91, S098125", "F566822121G", "2121G", "91112000");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("Jenny Smith Landi", "Blk 10, Paya Lebar St 2, Unit 017-31 , S700010", "F577821041H", "1041H", "92222000");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("He Wang Kang", "Blk 1, Chinatown St 37, Unit 01-123, S010001", "F587822349J", "2349J", "91020200");
+INSERT INTO heroku_e3d09080d56a833.residents (fullName, address, UFIN, UFINLastFiveDigit, mobileNo)VALUES ("Müller Maeyer", "Blk 5, Newton St 20, Unit 10-200, S100005", "F579529250K", "9250K", "98208282");
